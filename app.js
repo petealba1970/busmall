8f0fdc8518ff27aaf448e13f001f6b358a00a5a4
Pete Alba <petealba1970@gmail.com>
2019-11-13 12:58:30 -0800

    adding local storage

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 'use strict';
+var PIC_DATA = 'picData';
+
+
 var picStorage = [];
 var randomPics = [];
 var clickCounter = 0;
 var MAX_CLICK_COUNTER = 25;
+
 
 function getRandomPicIndex() {
   return Math.floor(Math.random() * (picStorage.length));
@@ -41,6 +45,7 @@ function select3PicsAndRender() {
 var Picture = function (name, picture) {
   this.name = name;
   this.picture = picture;
+  // we want to preserve these
   this.timesClicked = 0;
   this.timesShown = 0;
 
@@ -48,33 +53,69 @@ var Picture = function (name, picture) {
     this.timesClicked++;
   }
   this.render = function (domReference) {
-    domReference.src = picture;
+    domReference.src = this.picture;
   }
-  picStorage.push(this);
+  this.loadData = function (data) {
+    this.timesClicked = data.timesClicked;
+    this.timesShown = data.timesShown;
+    this.name = data.name;
+    this.picture = data.picture;
+  }
 }
 
+if (localStorage.getItem(PIC_DATA) === null) {
 
-var bagPic = new Picture('Bag', './pictures/bag.jpg');
-var bananaPic = new Picture('Banana', './pictures/banana.jpg')
-var bathroomPic = new Picture('Bathroom', './pictures/bathroom.jpg')
-var bootsPic = new Picture('Boots', './pictures/boots.jpg')
-var breakfastPic = new Picture('Breakfast', './pictures/breakfast.jpg');
-var bubblegumPic = new Picture('Bubble Gum', './pictures/bubblegum.jpg')
-var chairPic = new Picture('Chair', './pictures/chair.jpg')
-var cthulhuPic = new Picture('Cthulhu', './pictures/cthulhu.jpg')
-var dogduckPic = new Picture('Dog-Duck', './pictures/dog-duck.jpg');
-var dragonPic = new Picture('Dragon', './pictures/dragon.jpg')
-var penPic = new Picture('Pen', './pictures/pen.jpg')
-var petsweepPic = new Picture('Pet Sweep', './pictures/pet-sweep.jpg')
-var scissorsPic = new Picture('Scissors', './pictures/scissors.jpg');
-var sharkPic = new Picture('Shark', './pictures/shark.jpg')
-var sweepPic = new Picture('Sweep', './pictures/sweep.png')
-var tauntaunPic = new Picture('Tauntaun', './pictures/tauntaun.jpg')
-var unicornPic = new Picture('Unicorn', './pictures/unicorn.jpg');
-var usbPic = new Picture('USB', './pictures/usb.gif')
-var watercanPic = new Picture('Water Can', './pictures/water-can.jpg')
-var wineglassPic = new Picture('Wine Glass', './pictures/wine-glass.jpg')
+  var bagPic = new Picture('Bag', './pictures/bag.jpg');
+  var bananaPic = new Picture('Banana', './pictures/banana.jpg')
+  var bathroomPic = new Picture('Bathroom', './pictures/bathroom.jpg')
+  var bootsPic = new Picture('Boots', './pictures/boots.jpg')
+  var breakfastPic = new Picture('Breakfast', './pictures/breakfast.jpg');
+  var bubblegumPic = new Picture('Bubble Gum', './pictures/bubblegum.jpg')
+  var chairPic = new Picture('Chair', './pictures/chair.jpg')
+  var cthulhuPic = new Picture('Cthulhu', './pictures/cthulhu.jpg')
+  var dogduckPic = new Picture('Dog-Duck', './pictures/dog-duck.jpg');
+  var dragonPic = new Picture('Dragon', './pictures/dragon.jpg')
+  var penPic = new Picture('Pen', './pictures/pen.jpg')
+  var petsweepPic = new Picture('Pet Sweep', './pictures/pet-sweep.jpg')
+  var scissorsPic = new Picture('Scissors', './pictures/scissors.jpg');
+  var sharkPic = new Picture('Shark', './pictures/shark.jpg')
+  var sweepPic = new Picture('Sweep', './pictures/sweep.png')
+  var tauntaunPic = new Picture('Tauntaun', './pictures/tauntaun.jpg')
+  var unicornPic = new Picture('Unicorn', './pictures/unicorn.jpg');
+  var usbPic = new Picture('USB', './pictures/usb.gif')
+  var watercanPic = new Picture('Water Can', './pictures/water-can.jpg')
+  var wineglassPic = new Picture('Wine Glass', './pictures/wine-glass.jpg')
 
+  picStorage.push(bagPic);
+  picStorage.push(bananaPic);
+  picStorage.push(bathroomPic);
+  picStorage.push(bootsPic);
+  picStorage.push(breakfastPic);
+  picStorage.push(bubblegumPic);
+  picStorage.push(chairPic);
+  picStorage.push(cthulhuPic);
+  picStorage.push(dogduckPic);
+  picStorage.push(dragonPic);
+  picStorage.push(penPic);
+  picStorage.push(petsweepPic);
+  picStorage.push(scissorsPic);
+  picStorage.push(sharkPic);
+  picStorage.push(sweepPic);
+  picStorage.push(tauntaunPic);
+  picStorage.push(unicornPic);
+  picStorage.push(usbPic);
+  picStorage.push(watercanPic);
+  picStorage.push(wineglassPic);
+
+} else {
+  var jsonData = localStorage.getItem(PIC_DATA);
+  var data = JSON.parse(jsonData);
+  for (var i = 0; i < data.length; i++); {
+    var newPicture = new Picture('', '');
+    newPicture.loadData(data[i]);
+    picStorage.push(newPicture);
+  }
+}
 
 function clickManager(event) {
   clickCounter++;
@@ -94,11 +135,17 @@ function clickManager(event) {
 
     select3PicsAndRender();
   } else {
+    savePictureDataToLocalStorage();
     createPicChart();
+  }
+  function savePictureDataToLocalStorage() {
+    var jsonData = JSON.stringify(picStorage);
+    localStorage.setItem(PIC_DATA, jsonData);
   }
 }
 
 select3PicsAndRender();
+
 
 var placeholder0 = document.getElementById('placeholder-0');
 var placeholder1 = document.getElementById('placeholder-1');
@@ -162,3 +209,14 @@ function createPicChart() {
     },
   });
 }
+
+
+
+
+
+
+
+
+
+
+
